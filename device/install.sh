4672d8ca893fe80a2d1dd7e03bd4a8141b76db5e
erobinson <edward.r.robinson@gmail.com>
2014-05-04 16:44:31 -0400

    little more reoganizing after merge

--- device/install.sh
+++ device/install.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# MySQL should already be installed
-# ubuntu command to install: something like sudo apt-get install mysql
-#
-
-# run sql to create or update db
-# run the create_db.sql to create the db
-
-# install anything needed for the python shells
-# 	possibly export variables/config file
-
-# setup server as 
+# determine device type RPi/Silverlode waysmall comp
+# run basic setup/setup.sh
+# run device specific setup.sh
+# run database/setup.sh

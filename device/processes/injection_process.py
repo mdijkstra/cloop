0cd002211ff1e1ff250468f0ffeafe9585e89057
erobinson <edward.r.robinson@gmail.com>
2014-07-31 08:03:05 -0400

    complete first draft of injection_process (still need to debug) added cloop_config

--- device/processes/injection_process.py
+++ device/processes/injection_process.py
@@ -13,6 +13,7 @@ import time
 import logging
 import signal
 import datetime
+import cloop_config
 #from bluetooth import *
 #from time import sleep
 
@@ -25,7 +26,7 @@ logging.basicConfig(filename='./log/' + currentDate + '.log',level=logging.DEBUG
                 format='%(asctime)s %(levelname)s at %(lineno)s: %(message)s')
 
 
-class DeviceDBTransData():
+class injection_process():
   db_host = "localhost"
   db_port = 3306
   db_user = "root"
@@ -40,7 +41,7 @@ class DeviceDBTransData():
                     passwd=self.db_pass, \
                     db=self.db_db)
     self.db = self.db_conn.cursor()  
-    self.cloop_config = CloopConfig();
+    self.cloop_config = cloop_config();
 
   def __del__(self):
     self.db.close()
@@ -72,8 +73,7 @@ class DeviceDBTransData():
     cur_iob_units = self.get_cur_iob_units()
     cur_bg_units = self.get_cur_bg_units() #units over or under target bg
     carbs_to_cover = self.get_carbs_to_cover()
-    units_for_carbs = self.carbs_to_cover_units()
-    target_bg = self.cloop_config.get_target_bg()
+    units_for_carbs, courses_to_cover = self.carbs_to_cover_units()
     low_limit_units = self.cloop_config.get_low_limit_units()
     temp_duration = self.cloop_config.get_temp_duration()
     cur_basal_units = self.get_cur_basal_units(temp_duration)
@@ -83,135 +83,105 @@ class DeviceDBTransData():
     injection_units = None
 
     correction_units = cur_bg_units - cur_iob
-    if all_meal_carbs_abosrbed and cur_bg_units > low_limit_units and cur_bg_units < cur_iob_units)
+    if all_meal_carbs_abosrbed \
+                    and cur_bg_units > low_limit_units \
+                    and cur_bg_units < cur_iob_units:
        correction_units = 0
     
     injection_units = units_for_carbs + correction_units
     temp_rate = ( cur_basal_units + injection_units ) / temp_duration
+    temp_rate = temp_rate * (60 / temp_duration) # convert to units per hour
     # log all data into db
     # insert meals to injection records
+    self.mark_courses_for_injection(courses_to_cover, injection_id)
     return temp_rate
       
 
   # get the current Insulin On Board
   def get_cur_iob_units(self):
-    sql_select_iob = "select datetime_iob, iob from iob where datetime_iob = (select max(datetime_iob) from iob where datetime_iob < now() and datetime_iob > now()-10min)"
-    self.db.execute(sql_select_sgvs)
+    sql_select_iob = "select datetime_iob, iob from iob where \
+                    datetime_iob = (select max(datetime_iob) from iob \
+                    where datetime_iob < now() and datetime_iob > now()-10min)"
+    self.db.execute(sql_select_iob)
     iob = 0 
     for row in self.db.fetchall():
-      iob = row[1]
+      iob = row["iob"]
     return iob
 
   def get_cur_bg_units(self):
-
-  def get_target_bg(self):
+    sql_select_sgvs = "select datetime_recorded, sgv from sgv where \
+                    datetime_recorded = (select max(datetime_recorded) from sgv \
+                    where datetime_recorded < now() and datetime_iob > now()-20min)"
+    self.db.execute(sql_select_sgvs)
+    cur_bg = None 
+    for row in self.db.fetchall():
+      cur_bg = row["sgv"]
+    cur_bg_units = (cur_bg - self.oloop_config.get_target_bg()) / self.cloop_config.get_bg_sensitivity()
+    return cur_bg_units
 
   def get_carbs_to_cover_units(self):
+    units_for_carbs = 0
+    courses_to_cover = self.get_courses_to_cover()
+    carbs = 0
+    for course in courses_to_cover:
+      carbs += course["carbs"]
+    units_for_carbs = carbs / self.cloop_config.get_carb_sensitivity()
+    return units_for_carbs, courses_to_cover
 
-  def get_carbs_to_cover(self):
+  def get_courses_to_cover(self):
+    sql_get_courses = "select * from courses where \
+                    datetime_consumption > now()-45 and datetime_consumption < now()+50 \
+                    and course_id not in (select course_id from courses_to_injections where \
+                    injection_id in (select injection_id from injections where status = 'sucessful'))"
+    self.db.execute(sql_get_courses)
+    return self.db.fetchall()
+
+  def mark_courses_for_injection(self, courses, injection_id):
+    sql_to_mark = "insert into courses_to_injections (course_id, injection_id) values "
+    for course in courses:
+      sql_to_mark += "(" + course["course_id"] + ", " + injection_id + "),"
+    sql_to_mark = sql_to_mark[:1]
+    self.db.execute(sql_to_mark)
 
   def get_cur_basal_units(self):
+    return 1.1
 
   def get_all_meal_carbs_absorbed(self):
-
-  def get_carb_sensitivity(self):
-  
-  def get_bg_sensitivity(self):
+    sql_get_last_injection = "select * from injections where status = 'successful' and \
+                    injection_id in (select distinct injection_id from courses_to_injections) \
+                    order by datetime_delivered desc limit 1"
+    self.db.execute(sql_get_last_injection)
+    last_injection = None
+    for row in self.db.fetchall():
+      last_injection = row
+    if last_injection = None:
+      return True
+    if last_injection["datetime_delivered"] + 2 < now():
+      return True
+    else:
+      return False
 
   def get_current_bg(self):
-    return None
+    sql_select_sgvs = "select datetime_recorded, sgv from sgv where \
+                    datetime_recorded = (select max(datetime_recorded) from sgv \
+                    where datetime_recorded < now() and datetime_iob > now()-20min)"
+    self.db.execute(sql_select_sgvs)
+    cur_bg = None 
+    for row in self.db.fetchall():
+      cur_bg = row["sgv"]
+    return cur_bg
 
   def is_active_injection(self):
+    sql_get_active_injections = "select * from injections where \
+                    status = 'successful' and datetime_delivered > now()-31"
+    rows = self.db.fetchall()
+    if len(rows) > 0:
+      return True
+    else:
+      return False
 
-
-  ################### Internal methods below ############################
-  #switch from xml to json objects
-  def import_sgvs(self, svgs_xml):
-    print "TODO: Implement import_sgvs (" + svgs_xml + ")"
-    print "      Should not be needed as the device sources the sgvs from the pump"
-    print "      The API for importing data from the pump should probably be in a diff file"
-
-  def export_sgvs(self):
-    sql_select_sgvs = "select sgv_id, device_id, datetime_recorded, sgv from sgvs where transferred != 'yes'"
-    logging.info('Exporting SGValues: '+ sql_select_sgvs)
-    self.db.execute(sql_select_sgvs)
-    xml = "<sgvs>"
-    for row in self.db.fetchall():
-      xml+= "<sgv_record>"
-      xml += "<sgv_id>" + str(row[0]) + "</sgv_id>"
-      xml += "<device_id>" + str(row[1]) + "</device_id>"
-      xml += "<datetime_recorded>" + row[2].strftime(dateFormat) + "</datetime_recorded>"
-      xml += "<sgv>" + str(row[3]) + "</sgv>"
-      xml += "</sgv_record>"
-    xml += "</sgvs>"
-    logging.info('returning the following xml from export_sgvs:'+xml)
-    return xml
-
-  def export_sgvs_success(self):
-    sql_update_exported = "update sgvs set transferred = 'yes' where transferred = 'no'" 
-    logging.info('export_sgvs: updating transferred: '+sql_update_exported)
-    try:
-      self.db.execute(sql_update_exported)
-      self.db_conn.commit()
-    except:
-      self.db_conn.rollback()
-      logging.error("******* rolled back update : "+sql_update_exported)
-
-  def import_courses(self, courses_xml):
-    index = courses_xml.index("<courses>")
-    if courses_xml.index("<courses>") == 0:
-      courses_xml = get_value_from_xml(courses_xml, "courses")
-    if courses_xml == "":
-      return
-    courses = get_values_from_xml(courses_xml, "course")
-    for course_xml in courses:
-        insert_sql = course_xml_to_sql_insert(course_xml)
-        try:
-            # print "about to run : "+insert_sql
-            self.db.execute(insert_sql)
-            self.db_conn.commit()
-        except:
-            self.db_conn.rollback()
-            logging.error("******* rolled back insert : "+insert_sql)
-
-
-  def course_xml_to_sql_insert(self, course_xml):
-    course_id = get_value_from_xml(course_xml, "course_id")
-    food_id = get_value_from_xml(course_xml, "food_id")
-    serv_quantity = get_value_from_xml(course_xml, "serv_quantity")
-    carbs = get_value_from_xml(course_xml, "carbs")
-    datetime_consumption = get_value_from_xml(course_xml, "datetime_consumption")
-    datetime_ideal_injection = get_value_from_xml(course_xml, "datetime_ideal_injection")
-    comment = get_value_from_xml(course_xml, "comment")
-    # not needed on device, only needed on original location (app)
-    #    transferred = get_value_from_xml(xml, "transferred")
-    sql = " insert into courses (course_id, food_id, serv_quantity, carbs, \
-            datetime_consumption, datetime_ideal_injection, comment) \
-            values ( %d, %d, %f, %d, '%s','%s', '%s')" \
-            % (int(course_id), int(food_id), float(serv_quantity), int(carbs), \
-            datetime_consumption, datetime_ideal_injection, comment)
-
-    logging.info(" courses_xml_to_sql_insert : "+sql)
-    return sql
-
-'''
-########## Test import courses with empty data #######
-db_trans = DeviceDBTransData()
-db_trans.import_courses("<courses></courses>")
-'''
 
 
 if __name__ == '__main__':
-  logging.info('Going to try to sync device db and phone db...')
-  db_trans = DeviceDBTransData()
-  data_to_send = db_trans.get_data_to_send()
-  bt_trans = DeviceBTPhoneTransData()
-  data_from_phone = bt_trans.transfer(data_to_send)
-  if not data_from_phone is None:
-    logging.info('sucessfully transferred data from device to phone.')
-    db_trans.export_sgvs_success()
-    logging.info('main: going to import data from phone')
-    db_trans.import_data(data_from_phone)  
-  else:
-    logging.warning('no data from phone')
-  logging.info('DONE WITH PHONE SYNC\n\n\n\n')
+  process = injection_process()
+  process.process_injection()
